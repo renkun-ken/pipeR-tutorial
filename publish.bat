f2d5cd6243a6aa0679ca26d4ce66c325611156b7
Kun Ren <ken@renkun.me>
2015-01-09 20:25:58 +0800

    publisher

---
+++ publish.bat
@@ -0,0 +1,8 @@
+rscript -e "Rgitbook::buildGitbook()"
+cd _book
+git init
+git commit --allow-empty -m "Update build gitbook"
+git checkout -b gh-pages
+git add .
+git commit -am "Update build gitbook"
+git push https://github.com/renkun-ken/pipeR-tutorial.git gh-pages --force
